7907ac94b4eb5675d371e3cd443848c654ffcaed
hoon105204 <hoon105204@gmail.com>
2022-08-20 16:15:47 +0900

    backend setting & homepage

---
+++ backend/kurly/src/main/java/com/kurly/demo/KurlyApplication.java
@@ -0,0 +1,21 @@
+package com.kurly.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class KurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String gohome() {
+		return "home";
+	}
+
+}
