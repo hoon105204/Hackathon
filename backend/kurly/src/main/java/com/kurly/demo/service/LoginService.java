f96f49501264d345ad43b2e33f2dbf7e81105669
hoon105204 <hoon105204@gmail.com>
2022-08-21 20:50:01 +0900

    login

--- backend/kurly/src/main/java/com/kurly/demo/service/LoginService.java
+++ backend/kurly/src/main/java/com/kurly/demo/service/LoginService.java
@@ -1,5 +1,8 @@
 package com.kurly.demo.service;
 
-public interface LoginService {
+import com.kurly.demo.dto.TB_CM_EMP;
 
+public interface LoginService {
+	public TB_CM_EMP userID(TB_CM_EMP dto);
+	public int insertUser(TB_CM_EMP dto);
 }
--- backend/kurly/src/main/java/com/kurly/demo/service/LoginServiceImpl.java
+++ backend/kurly/src/main/java/com/kurly/demo/service/LoginServiceImpl.java
@@ -1,8 +1,25 @@
 package com.kurly.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kurly.demo.dto.TB_CM_EMP;
+import com.kurly.demo.mapper.LoginMapper;
 
 @Service
 public class LoginServiceImpl implements LoginService{
+	
+	@Autowired
+	LoginMapper loginMapper;
+
+	@Override
+	public TB_CM_EMP userID(TB_CM_EMP dto) {
+		return loginMapper.userID(dto);
+	}
+
+	@Override
+	public int insertUser(TB_CM_EMP dto) {
+		return loginMapper.insertUser(dto);
+	}
 
 }
