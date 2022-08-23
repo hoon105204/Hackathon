0e66f1c93146d81746e1ec97142e6f38c10f69f8
BiohPark <qldh1654@gmail.com>
2022-08-23 23:14:11 +0900

    작업목록 완료

--- backend/kurly/src/main/java/com/kurly/demo/service/WrkService.java
+++ backend/kurly/src/main/java/com/kurly/demo/service/WrkService.java
@@ -3,7 +3,9 @@ package com.kurly.demo.service;
 import java.util.List;
 
 import com.kurly.demo.dto.TB_LO_WORK;
+import com.kurly.demo.dto.TB_LO_WORKDTL;
 
 public interface WrkService {
 	public List<TB_LO_WORK> getWrkList(TB_LO_WORK dto);
+	public List<TB_LO_WORKDTL> getWrkDtlList(TB_LO_WORKDTL dto);
 }
